0602a7168a9ded1507392633f44639067e960a0a
manuelechavarria <maechavarriaor@gmail.com>
2026-06-09 16:26:45 -0500

    Video

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -38,7 +38,7 @@
       .col-lg-12
         figure(data-aos="fade-down")
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/vdPrCjWJSHo?si=X4NxENRo3LLXAua_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/6YzoHqVc2GU?si=Hvkd6MN14Ebtlv53" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
 
 
